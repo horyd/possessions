a3062563b156385e6ced3f50b44e320f74285692
horyd <prods3@gmail.com>
2016-06-06 10:58:47 +0200

    babel polyfill reqd

--- tools/deploy.gh.js
+++ tools/deploy.gh.js
@@ -7,7 +7,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
-
+require('babel-polyfill');
 const GitRepo = require('git-repository');
 const task = require('./task');
 
